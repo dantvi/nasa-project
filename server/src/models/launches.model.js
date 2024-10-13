765d116454cdaf44586b3e31ab5f5e5caa622f0d
dantvi <daniel.tving@gmail.com>
2024-10-13 11:53:49 +0200

    creating launches

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,5 +1,7 @@
 const launches = new Map();
 
+let latestFlightNumber = 100;
+
 const launch = {
   flightNumber: 100,
   mission: 'Keppler Exploration X',
@@ -14,9 +16,22 @@ const launch = {
 launches.set(launch.flightNumber, launch);
 
 function getAllLaunches() {
-  return Array.from(launches.clear.values());
+  return Array.from(launches.values());
+}
+
+function addNewLaunch(launch) {
+  latestFlightNumber++;
+  launches.set(
+    latestFlightNumber, 
+    Object.assign(launch, {
+      flightNumber: latestFlightNumber,
+      customer: ['ZTM', 'NASA'],
+      upcoming: true,
+      success: true
+  }));
 }
 
 module.exports = {
   getAllLaunches,
+  addNewLaunch
 }
